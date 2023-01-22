70b17b56bed2aef6adfa7405809f7d19ad763bb0
manav1411 <41612145+manav1411@users.noreply.github.com>
2023-01-22 15:51:51 +1100

    Add files via upload

--- my_functions.js
+++ my_functions.js
@@ -2,9 +2,23 @@
 function mystery_button() {
     var x = document.getElementById("boring_text");
 
+    var circle1 = document.getElementById("circle1");
+    var circle2 = document.getElementById("circle2");
+    var circle3 = document.getElementById("circle3");
+    var circle4 = document.getElementById("circle4");
+
+
     if (x.innerHTML === "yay! exciting ^_^") {
       x.innerHTML = "smh - so boring!!";
+      circle1.innerHTML = "";
+      circle2.innerHTML = "";
+      circle3.innerHTML = "";
+      circle4.innerHTML = "";
     } else {
       x.innerHTML = "yay! exciting ^_^";
+      circle1.innerHTML = "about me";
+      circle2.innerHTML = "definitions";
+      circle3.innerHTML = "github";
+      circle4.innerHTML = "resources";
     }
   }
